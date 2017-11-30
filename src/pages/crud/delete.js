7e8b3adcd2c3b296b1f1065e2d377e027fa7b0d3
Troy Johnson <tjohnson@berkadia.com>
2017-11-30 16:34:29 -0700

    Worked on delete mutation

--- src/pages/crud/delete.js
+++ src/pages/crud/delete.js
@@ -1,16 +1,64 @@
-import React, { Component } from 'react';
+// React
+import React, { Component } from 'react' 
+
+// GraphQL & graph.cool
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+// CSS
 import '../../App.css';
 
+// https://www.apollographql.com/docs/react/basics/mutations.html#graphql-mutation-options
+
+const MUTATION = gql`
+  mutation deleteMedia($id: String!) {
+    deleteMedia(id: $id) {
+      id
+    }
+  }  
+`
+
 class Delete extends Component {
+  
+  onClick() {
+    this.props.mutate({
+      variables: { repoFullName: 'apollographql/apollo-client' }
+    })}}
+  
   render() {
-    return (
-      <div>
-        <h2>Delete</h2>
-        <input type="text" id="delete" placeholder='Enter ID'/>
-        <label for="delete"><button onClick=''>Delete!</button></label>
-      </div>
-    );
-  }
+
+const Delete = (props) => {
+
+  return (
+    <div>
+      <h2>Delete</h2>
+      <input type="text" id="delete" placeholder='Enter ID'/>
+      <label for="delete"><button onClick={this.onClick.bind(this)}>Delete!</button></label>
+    </div>
+  )
+
 }
 
-export default Delete;
+export default graphql(MUTATION)(Delete)
+
+const Delete = (props) => {
+
+  return (
+    <div>
+      <h2>Delete</h2>
+      <input type="text" id="delete" placeholder='Enter ID'/>
+      <label for="delete"><button onClick={e => props.mutate({
+        variables: {
+          id: cjamtxrrp77zd01178g2vmwao
+        }
+      })}>Delete!</button></label>
+    </div>
+  )
+
+}
+
+class NewEntry extends Component {
+  onClick() {
+    this.props.mutate({
+      variables: { repoFullName: 'apollographql/apollo-client' }
+    })}}
