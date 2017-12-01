ca9ceee5bb5f288b117bf19d78a0e4eac64bb05b
Troy Johnson <tjohnson@berkadia.com>
2017-12-01 13:10:53 -0700

    Corrected delete mutation

--- src/pages/crud/delete.js
+++ src/pages/crud/delete.js
@@ -1,64 +1,47 @@
 // React
 import React, { Component } from 'react' 
 
-// GraphQL & graph.cool
+// GraphQL
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
 // CSS
 import '../../App.css';
 
-// https://www.apollographql.com/docs/react/basics/mutations.html#graphql-mutation-options
+class Delete extends Component {
+
+  onClick() {
+    this.props.mutate({
+      variables: { id: '' }
+    })
+      .then(({ data }) => {
+        console.log('got data', data);
+      }).catch((error) => {
+        console.log('there was an error sending the query', error);
+      });
+  }
+  
+  render() {
+
+    return (
+
+      <div>
+        <h2>Delete</h2>
+        <input type='text' id='delete' placeholder='Enter ID'/>
+        <label for='delete'><button onClick={this.onClick.bind(this)} type='submit'>Delete!</button></label>
+      </div>
+    )
+  }
+}
 
 const MUTATION = gql`
-  mutation deleteMedia($id: String!) {
+  mutation deleteMedia($id: ID!) {
     deleteMedia(id: $id) {
       id
+      name
+      yearReleased
     }
   }  
 `
 
-class Delete extends Component {
-  
-  onClick() {
-    this.props.mutate({
-      variables: { repoFullName: 'apollographql/apollo-client' }
-    })}}
-  
-  render() {
-
-const Delete = (props) => {
-
-  return (
-    <div>
-      <h2>Delete</h2>
-      <input type="text" id="delete" placeholder='Enter ID'/>
-      <label for="delete"><button onClick={this.onClick.bind(this)}>Delete!</button></label>
-    </div>
-  )
-
-}
-
 export default graphql(MUTATION)(Delete)
-
-const Delete = (props) => {
-
-  return (
-    <div>
-      <h2>Delete</h2>
-      <input type="text" id="delete" placeholder='Enter ID'/>
-      <label for="delete"><button onClick={e => props.mutate({
-        variables: {
-          id: cjamtxrrp77zd01178g2vmwao
-        }
-      })}>Delete!</button></label>
-    </div>
-  )
-
-}
-
-class NewEntry extends Component {
-  onClick() {
-    this.props.mutate({
-      variables: { repoFullName: 'apollographql/apollo-client' }
-    })}}
