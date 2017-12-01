259a528337065190a3eb401dad0ed820db374db8
Troy Johnson <tjohnson@berkadia.com>
2017-12-01 16:55:41 -0700

    Fixed delete mutation

--- src/pages/crud/delete.js
+++ src/pages/crud/delete.js
@@ -10,50 +10,29 @@ import '../../App.css';
 
 class Delete extends Component {
 
-  constructor() {
-    super();
-    this.state = {
+  state = {
       id: '',
-    }
-  }
-
-  onChange = (e) => {
-    const state = this.state
-    state[e.target.name] = e.target.value
-    this.setState(state)
   }
 
   onSubmit = (e) => {
-    const { id } = this.state
+    e.preventDefault()
     this.props.mutate({
-      variables: { id: id }
+      variables: { id: this.state.id }
     })
       .then(({ data }) => {
         console.log('got data', data)
-      }).catch((error) => {
+    }).catch((error) => {
         console.log('there was an error sending the query', error)
-      })
+    })
   }
 
-  // onClick() {
-  //   this.props.mutate({
-  //     variables: { id: this.state.id }
-  //   })
-  //     .then(({ data }) => {
-  //       console.log('got data', data);
-  //     }).catch((error) => {
-  //       console.log('there was an error sending the query', error);
-  //     });
-  // }
-  
   render() {
     const { id }  = this.state
     return (
       <div>
         <h2>Delete</h2>
         <form onSubmit={this.onSubmit}>
-          <input type='text' name='id' value={id} onChange={this.onChange} placeholder='Enter ID'/>
-          {/* <button onClick={this.onClick.bind(this)} type='submit'>Delete!</button> */}
+          <input type='text' name='id' value={id} onChange={e => this.setState({ id: e.target.value })} placeholder='Enter ID'/>
           <button type='submit'>Submit</button>
         </form>
       </div>
