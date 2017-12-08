332ed2c9f4eb233e365623d33c9b2be7c7046d61
Troy Johnson <troy.johnson@berkadia.com>
2017-12-08 16:44:16 -0700

    Changed admin page to display CUD w/ modals

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -12,7 +12,52 @@ class Admin extends Component {
           <h1 className='display-3'>Admin</h1>
           <main>
           
-            <p>
+            <button type="button" className="btn btn-info" data-toggle="modal" data-target="#createModal">Create</button>
+            <div className="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModalTitle" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="createModalTitle">Create</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body"><Create /></div>
+                </div>
+              </div>
+            </div>
+
+            <button type="button" className="btn btn-info" data-toggle="modal" data-target="#updateModal">Update</button>
+            <div className="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="updateModalTitle" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="updateModalTitle">Update</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body"><Update /></div>
+                </div>
+              </div>
+            </div>
+
+            <button type="button" className="btn btn-info" data-toggle="modal" data-target="#deleteModal">Delete</button>
+            <div className="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalTitle" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="deleteModalTitle">Delete</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body"><Delete /></div>
+                </div>
+              </div>
+            </div>
+
+            {/* <p>
               <button className="btn btn-info" type="button" data-toggle="collapse" data-target="#create-collapse" aria-expanded="false" aria-controls="create-collapse">Create</button>
               <button className="btn btn-info" type="button" data-toggle="collapse" data-target="#update-collapse" aria-expanded="false" aria-controls="update-collapse">Update</button>
               <button className="btn btn-info" type="button" data-toggle="collapse" data-target="#delete-collapse" aria-expanded="false" aria-controls="delete-collapse">Delete</button>
@@ -35,7 +80,7 @@ class Admin extends Component {
                 <h3>Delete</h3>
                 <Delete />
               </div>
-            </div>
+            </div> */}
 
           {/* <ul className="nav nav-tabs" id="myTab" role="tablist">
             <li className="nav-item">
