a734f631b73675bc20c050c14fa47faab3c77dda
Troy Johnson <troy.johnson@berkadia.com>
2017-12-08 16:45:44 -0700

    Cleaned up admin.js code

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -57,48 +57,6 @@ class Admin extends Component {
               </div>
             </div>
 
-            {/* <p>
-              <button className="btn btn-info" type="button" data-toggle="collapse" data-target="#create-collapse" aria-expanded="false" aria-controls="create-collapse">Create</button>
-              <button className="btn btn-info" type="button" data-toggle="collapse" data-target="#update-collapse" aria-expanded="false" aria-controls="update-collapse">Update</button>
-              <button className="btn btn-info" type="button" data-toggle="collapse" data-target="#delete-collapse" aria-expanded="false" aria-controls="delete-collapse">Delete</button>
-            </p>
-
-            <div className="collapse" id="create-collapse">
-              <div className="card card-body">
-                <h3>Create</h3>
-                <Create />
-              </div>
-            </div>
-            <div className="collapse" id="update-collapse">
-              <div className="card card-body">
-                <h3>Update</h3>
-                <Update />
-              </div>
-            </div>
-            <div className="collapse" id="delete-collapse">
-              <div className="card card-body">
-                <h3>Delete</h3>
-                <Delete />
-              </div>
-            </div> */}
-
-          {/* <ul className="nav nav-tabs" id="myTab" role="tablist">
-            <li className="nav-item">
-              <a className="nav-link" id="create-tab" data-toggle="tab" href="#create" role="tab" aria-controls="home" aria-selected="true"><b>Create</b></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" id="update-tab" data-toggle="tab" href="#update" role="tab" aria-controls="profile" aria-selected="false"><b>Update</b></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" id="delete-tab" data-toggle="tab" href="#delete" role="tab" aria-controls="contact" aria-selected="false"><b>Delete</b></a>
-            </li>
-          </ul>
-          <div className="tab-content" id="myTabContent">
-            <div className="tab-pane fade" id="create" role="tabpanel" aria-labelledby="create-tab"><Create /></div>
-            <div className="tab-pane fade" id="update" role="tabpanel" aria-labelledby="update-tab"><Update /></div>
-            <div className="tab-pane fade" id="delete" role="tabpanel" aria-labelledby="delete-tab"><Delete /></div>
-          </div> */}
-
           </main>  
         </div>
       </div>  
