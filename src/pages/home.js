5cc92aaeac869209aef19d1bf6ba422cf7f10d91
Troy Johnson <troy.johnson57@gmail.com>
2018-01-03 18:20:29 -0700

    Updated login card

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,25 +17,30 @@ class Home extends Component {
     return (
       <div className='container cover-container'>
         <div className='jumbotron'>
-        <div className='row'>
-          <div className='col-md'>
-            <h1 className="cover-heading">Isotope</h1>
-            <p className="lead">is a tool to catalogue the movies and tv shows that you own (regardless of format).</p>
-          </div>
-          <div className='col-md container-fluid'>
-            <form onSubmit={this.onSubmit}>
-              <div className='row'>
-                <div className='col'>
-                  <input className='form-control' type='text' name='name' value={username} onChange={e => this.setState({ username: e.target.value })} placeholder='Username' required />
-                </div>
-                <div className='col'>
-                  <input className='form-control' type='password' name='password' value={password} onChange={e => this.setState({ password: e.target.value })} placeholder='Password' required />
-                </div>
+          <div className='row'>
+            <div className='col-md'>
+              <h1 className="display-1">Isotope</h1>
+              <p className="lead">...is a tool to catalogue the movies and tv shows that you own (regardless of format).</p>
+            </div>
+            <div className='col-md card card-login'>
+              <div className='card-top'>
+                <h2>Login</h2>
               </div>
-              <button className='btn btn-info' type='submit'>Login</button>
-            </form>
+              <div className='card-body'>
+                <form onSubmit={this.onSubmit}>
+                  <div className='row'>
+                    <div className='col'>
+                      <input className='form-control' type='text' name='name' value={username} onChange={e => this.setState({ username: e.target.value })} placeholder='Username' required />
+                    </div>
+                    <div className='col'>
+                      <input className='form-control' type='password' name='password' value={password} onChange={e => this.setState({ password: e.target.value })} placeholder='Password' required />
+                    </div>
+                  </div>
+                  <button className='btn btn-info' type='submit'>Login</button>
+                </form>
+              </div>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     );
